327f4bcb90501374388e2df9634ad8d0de456ee8
Ali Akbar Khan <122238636+aliakbarkhan-webdeveloper@users.noreply.github.com>
2024-07-14 22:03:09 +0500

    challenge 2

---
+++ challenge2.js
@@ -0,0 +1,51 @@
+//Activity 1: Arithmetic Operators
+
+/*
+//task 1: add two number
+function add(num1,num2){
+    console.log(num1+num2);
+}
+add(3,7)
+
+//Task 2: Subtract two number
+function sub(num1,num2) {
+    console.log(num1-num2);
+}
+sub(7,5)
+
+//Task 3: Multiply two numbers
+function multi(num1,num2) {
+    console.log(num1*num2);
+}
+multi(4,7)
+
+//Task 4: divide two number
+function divide(num1,num2) {
+    console.log(num1/num2);
+}
+divide(10,2)
+
+//task 5: find reminder
+function reminder(num1,num2) {
+    console.log(num1%num2);
+}
+reminder(10,2);
+*/
+
+
+/*
+//Activity 2: Assignment Operators
+
+//Task 1: add number to varible
+// let a=6;
+// a+=4;
+// console.log(a);
+
+//Task 2: Subtract
+// let a=10;
+// a-=5;
+// console.log(a);
+
+*/
+
+
