635f71610632be8b18adba36acfac039a8cc9429
Ali Akbar Khan <122238636+aliakbarkhan-webdeveloper@users.noreply.github.com>
2024-07-14 21:39:33 +0500

    commit

---
+++ challenge1.js
@@ -0,0 +1,39 @@
+// //Activity 1:
+// var a=5;
+// let b="str"
+// console.log(a);
+// console.log(b);
+
+// Activity 2: constant
+
+// const bool=true;
+// console.log(bool);
+
+//Activity 3: Data types
+
+// let a=1;
+// let b="str";
+// let c=false;
+// let d={};
+// let e=[1];
+// console.log(typeof a);
+// console.log(typeof b);
+// console.log(typeof c);
+// console.log(typeof d);
+// console.log(typeof e);
+
+//Activity 4: reassigning values to let var
+// let a=67;
+// console.log(a);
+// a=6
+// console.log(a);
+// console.log(a);
+// var a=8;
+// console.log(a);
+// a=9
+// console.log(a);
+
+//Activity 5: Const variable: reassign a value
+// const a=9;
+// a=8
+// console.log(a);
