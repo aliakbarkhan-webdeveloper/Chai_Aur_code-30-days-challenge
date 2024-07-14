4b50872c583922010974098c2f6d814bc01be45f
Ali Akbar Khan <122238636+aliakbarkhan-webdeveloper@users.noreply.github.com>
2024-07-14 23:51:44 +0500

    challenge 2 Completed

--- challenge2.js
+++ challenge2.js
@@ -32,7 +32,6 @@ function reminder(num1,num2) {
 reminder(10,2);
 */
 
-
 /*
 //Activity 2: Assignment Operators
 
@@ -48,8 +47,88 @@ reminder(10,2);
 
 */
 
-
+/*
 //Activity 4:  Comparison Operators
 
-//Task 1:
+//Task 1: compare two numbers with < and>
+// function compare(num1, num2) {
+//   if (num1 > num2) {
+//     return num1;
+//   } else {
+//     return num2;
+//   }
+// }
+// console.log(compare(5, 1));
+
+//Task 2: compre two num by using <= and >=
+// function compare(num1, num2) {
+// if (num1<=num2) {
+//     console.log(`number 2 is bigger`);
+// }else{
+//     console.log(`number 1 is bigger`);
+// }
+
+// }
+// compare(4,9)
+
+//Task 3:  compare by using === and ==
+
+// function compare(num1, num2) {
+//   if (num1 === num2) {
+//     console.log(`same type and equal`);
+//     return;
+//   }
+//   if (num1 == num2) {
+//     console.log(`equals but not same type`);
+//   }
+// }
+
+// compare(3,3)
+
+*/
+
+/*
+//Activity 4: logocal Operators
+
+//task 1: use && to combine two conditions
+// function logic(num1, num2) {
+//     let a=0;
+//   if (typeof num1 === typeof a && typeof num2===typeof a) {
+//     console.log(num1+num2);
+//   }else{
+//     console.log("not a number");
+//   }
+// }
+// logic(4,9)
+
+//task 2: use || to combine two conditions
+// function logic(num1, num2) {
+//     let a=0;
+//   if (typeof num1 === typeof a || typeof num2===a) {
+//     let b=num1+num2;
+//     console.log(b);
+//   }else{
+//     console.log("does not cintains any number");
+//   }}
+//   logic(2,"o")
+
+//task 3: use ! to negate a condition
+// function logic(num1) {
+//     let a=0;
+//   if (!num1) {
+//     console.log("value is false");
+//   }else{
+//     console.log("value is true");
+//   }}
+  
+//   logic(false)*/
+
+
+// Activity 5: ternary operator to check +ve number
+
+function ternary(num1) {
+  return num1 > 0 ? "positive" : "negative";
+}
+
+console.log(ternary(-5));
 
