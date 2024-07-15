a43837f5aa257e8d9a7c1d379801fd14fbb10911
Ali Akbar Khan <122238636+aliakbarkhan-webdeveloper@users.noreply.github.com>
2024-07-15 20:22:48 +0500

    7 days task completed

--- challenge3.js
+++ challenge3.js
@@ -47,6 +47,35 @@ function bigNum(num1, num2, num3) {
 bigNum(15, 29, 7);
 */
 
-
 //Activity 3: Switch Case
 
+//Task 1: determine the day if the week
+
+// let day = "wednesday";
+// switch (day) {
+//   case "monday":
+//     console.log("today is monday");
+//     break;
+
+//   case "tuesday":
+//     console.log("today is tuesday");
+//     break;
+//   case "wednesday":
+//     console.log("today is wednesday");
+//     break;
+//   case "thursday":
+//     console.log("today thursday");
+//     break;
+//   case "friday":
+//     console.log("today is friday");
+//     break;
+//   case "saturday":
+//     console.log("today is saturday");
+//     break;
+//   case "sunday":
+//     console.log("today is sunday");
+//     break;
+//   default:
+//     console.log("please choose the Right day");
+//     break;
+// }
