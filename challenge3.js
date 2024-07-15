db439762966784ba97c3fa06a3df3991cb425272
Ali Akbar Khan <122238636+aliakbarkhan-webdeveloper@users.noreply.github.com>
2024-07-15 22:44:33 +0500

    Some errors in switch should be solved

--- challenge3.js
+++ challenge3.js
@@ -81,29 +81,31 @@ bigNum(15, 29, 7);
 // }
 
 //Task 2: Grade System based of score
-let score = 90;
-let A = score >= 90 && score <= 100;
-let B = score >= 75 && score <= 89;
-let C = score <= 74 && score >= 60;
-let D = score >= 50 && score <= 59;
-let F = score <= 49;
-switch (score) {
-  case A:
-    console.log("A grade");
-    break;
-  case B:
-    console.log("B grade");
-    break;
-  case C:
-    console.log("B grade");
-    break;
-  case D:
-    console.log("B grade");
-    break;
-  case F:
-    console.log("B grade");
-    break;
-  default:
-    console.log("choose less of equal to 100");
-    break;
-}
+// let score = 1;
+// let A = score >= 90 && score <= 100;
+// let B = score >= 75 && score <= 89;
+// let C = score <= 74 && score >= 60;
+// let D = score >= 50 && score <= 59;
+// let F = score <= 49;
+// switch (score) {
+//   case A:
+//     console.log("A grade");
+//     break;
+//   case B:
+//     console.log("B grade");
+//     break;
+//   case C:
+//     console.log("C grade");
+//     break;
+//   case D:
+//     console.log("D");
+//     break;
+//   case F:
+//     console.log("bsdk PUBG band krdy");
+//     break;
+//   default:
+//     console.log("choose less of equal to 100");
+//     break;
+// }
+
+
