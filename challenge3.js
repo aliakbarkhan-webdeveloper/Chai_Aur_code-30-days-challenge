53a73aa73008a8f7c2e87fa0888cc5e261cee677
Ali Akbar Khan <122238636+aliakbarkhan-webdeveloper@users.noreply.github.com>
2024-07-15 20:03:52 +0500

    task one competed

--- challenge3.js
+++ challenge3.js
@@ -1,1 +1,26 @@
+/*
 //Activity 1: if-else Statement
+
+//Task 1: check a number is positive negative or zero;
+
+// function numCheck(num) {
+//   if (num > 0) {
+//     console.log("number is positive");
+//   } else if (num < 0) {
+//     console.log("number is negative");
+//   } else {
+//     console.log("number is neutral");
+//   }
+// }
+
+// numCheck(0);
+
+//task 2: check a person is eligible for vote, age>=18
+// function vote(age) {
+//   if (age >= 18) {
+//     console.log("voter is eligible for voting");
+//   } else {
+//     console.log("voter is under 18 and cannot cast vote");
+//   }
+// }
+// vote(18);*/
