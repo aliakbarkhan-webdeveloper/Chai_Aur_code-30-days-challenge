379389ec606d6afc04b4d8b06e8121e944102b5f
Ali Akbar Khan <122238636+aliakbarkhan-webdeveloper@users.noreply.github.com>
2024-07-17 12:27:29 +0500

    factorial uncompleted

--- Day4/dowhileloop.js
+++ Day4/dowhileloop.js
@@ -5,4 +5,16 @@
 //     count++
 // } while (count<6);
 
-
+//Task 2: factorial of a number
+// let count = 0;
+// let n = 5;
+// let factorial = 1;
+// let fraction = 1;
+// factorial *= fraction;
+// do {
+//   fraction = n - count;
+//   console.log(fraction);
+//   count++;
+// } while (count >= n);
+// console.log(n * factorial);
+// console.log(5 * 4 * 3 * 2 * 1);
