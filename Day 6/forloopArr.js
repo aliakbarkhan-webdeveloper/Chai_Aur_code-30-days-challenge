6d44e955b25d232a8a344941ec25ff6e0503d8f4
Ali Akbar Khan <122238636+aliakbarkhan-webdeveloper@users.noreply.github.com>
2024-07-18 23:38:58 +0500

    commit

--- Day 6/forloopArr.js
+++ Day 6/forloopArr.js
@@ -1,6 +1,5 @@
 //iterate array and log each element one by one
-let arr=[4,5,6,7,8,9];
+let arr = [4, 5, 6, 7, 8, 9];
 for (let i = 0; i < arr.length; i++) {
-    console.log(arr[i]);
-    
+  console.log(arr[i]);
 }
