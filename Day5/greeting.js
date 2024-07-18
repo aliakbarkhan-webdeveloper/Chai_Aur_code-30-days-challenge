4307e7a5e3e7d7be6d8ae4e625a36530ec333734
Ali Akbar Khan <122238636+aliakbarkhan-webdeveloper@users.noreply.github.com>
2024-07-18 17:08:43 +0500

    greeting function

--- Day5/greeting.js
+++ Day5/greeting.js
@@ -1,9 +1,7 @@
-function greet(name, age) {
-  let str = "";
-  if (typeof name !== typeof str || name == null) {
+function greet(name, age=18) {
+  
+  if (typeof name !== typeof "" || name == null) {
     console.log("name should be in string");
-  } else if (typeof age !== typeof 0 || age == null) {
-    console.log("age should be in number");
   } else {
     console.log(`hello ${name}, how are you?`);
   }
