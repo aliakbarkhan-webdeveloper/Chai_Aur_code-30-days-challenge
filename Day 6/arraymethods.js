d0269fefef9a4b470da8fe9e28a4b997f1dc18f0
Ali Akbar Khan <122238636+aliakbarkhan-webdeveloper@users.noreply.github.com>
2024-07-18 22:54:03 +0500

    array methods challenge

---
+++ Day 6/arraymethods.js
@@ -0,0 +1,14 @@
+let arr = [1, 2, 3, 4, 5];
+//push
+arr.push(6);
+console.log(arr);
+//pop
+arr.pop();
+console.log(arr);
+//shift
+arr.shift();
+console.log(arr);
+
+//unshift
+arr.unshift(0);
+console.log(arr);
