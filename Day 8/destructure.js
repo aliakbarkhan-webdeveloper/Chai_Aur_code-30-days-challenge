75e60d2f972e3ca521ad75fc9c1277283bb3118d
Ali Akbar Khan <122238636+aliakbarkhan-webdeveloper@users.noreply.github.com>
2024-07-27 17:17:42 +0500

    Activity 2 Completed

--- Day 8/destructure.js
+++ Day 8/destructure.js
@@ -1,8 +1,13 @@
-let arr=[3,6,8,9,3];
-let [index_0,index_1,index_2,index_3,index_4]=arr;
+let arr = [3, 6, 8, 9, 3];
+let [index_0, index_1, index_2, index_3, index_4] = arr;
 
-console.log(index_0,index_1);
+console.log(index_0, index_1);
 
+//Task 4
 
+function rest(a, b, c, d, e) {
+  return a + b + c + d + e;
+}
 
-
+let result = rest(...arr);
+console.log(result);
