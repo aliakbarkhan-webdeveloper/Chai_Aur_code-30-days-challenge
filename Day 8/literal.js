4dab3ca67d62e1bed8f4c3ea07b8de289c3d9a19
Ali Akbar Khan <122238636+aliakbarkhan-webdeveloper@users.noreply.github.com>
2024-07-27 17:10:42 +0500

    task 1 & 2

---
+++ Day 8/literal.js
@@ -0,0 +1,12 @@
+//task 1
+let name = "ali",
+  age = 24;
+console.log(`My name is ${name} and i am ${age} year old`);
+console.log(`
+    `);
+//Task 2
+console.log(`Multi-line string:
+This string is created with literal
+this is line 2
+this is line 3
+multiline string created with es6 template literal `);
