632eb3487e562737925f3710cfa6807425773e12
Ali Akbar Khan <122238636+aliakbarkhan-webdeveloper@users.noreply.github.com>
2024-07-27 15:33:50 +0500

    4 tasks completed

--- Day 7/bookObj.js
+++ Day 7/bookObj.js
@@ -1,10 +1,32 @@
 //Tasl 1
-let bookObj={
-    title:"MySelf",author:"Ali",year:Date()
+let bookObj = {
+  title: "MySelf",
+  author: "Ali",
+  year: Date(),
+  data: function () {
+    let title = this.title;
+    let author = this.author;
+    return { title, author };
+  },
+updateYear:function (year) {
+    this.year=year;
+
 }
+};
 console.log(bookObj);
 
 //Task 2
 
 console.log(bookObj.title);
 console.log(bookObj.author);
+
+//Task 3:
+
+console.log(bookObj.data());
+
+//Task 4
+
+bookObj.updateYear(1999);
+console.log(bookObj);
+
+
