d0ef50cbf065d92f411b1b1b71bfe7b3744427c7
Ali Akbar Khan <122238636+aliakbarkhan-webdeveloper@users.noreply.github.com>
2024-07-27 15:55:39 +0500

    Task 8 completed

--- Day 7/objloop.js
+++ Day 7/objloop.js
@@ -1,7 +1,9 @@
-let obj={
-    title:"Maths",author:"Ali Akbar",year:"2000"
-}
+let obj = {
+  title: "Maths",
+  author: "Ali Akbar",
+  year: "2000",
+};
 console.log("key : value");
 for (const key in obj) {
-   console.log(key+":" + obj[key]);
+  console.log(key + ":" + obj[key]);
 }
